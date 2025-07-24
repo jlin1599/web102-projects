37fe58cf191526de8eeab195f3a7dfe162ba75c1
Jackie Lin <JLin226859643@gmail.com>
2025-07-24 13:01:37 -0400

    env

--- crewmates-app/src/supabaseClient.js
+++ crewmates-app/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://phdmjisojymhvelxpofw.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoZG1qaXNvanltaHZlbHhwb2Z3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNzMyNTgsImV4cCI6MjA2ODk0OTI1OH0.lR8o0NoDbGUyGxUE9XsMlndr9GZEywkkBvJ8jax2b1g';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
